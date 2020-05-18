b2a5606eef828bdab8333c2fd4def4039e204622
piotrfer <piotrek99f@gmail.com>
2020-05-18 22:09:20 +0200

    basic activity model

---
+++ prodcalendar/apps/activity/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+class Activity(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    duration = models.IntegerField()
+    start = models.TimeField()
+    
+    categories = (
+        ('WRK', 'work'),
+        ('SCH', 'school'),
+        ('HOB', 'hobby'),
+        ('SOC', 'social'),
+        ('NED', 'needs'),
+        ('FUN', 'fun'),
+        ('REL', 'relax')
+    )
+        
+    days = (
+        ('MON','monday'),
+        ('TUE','tuesday'),
+        ('WED','wednesday'),
+        ('THU','thursday'),
+        ('FRI','friday'),
+        ('SAT','saturday'),
+        ('SUN','sunday')
+    )
+    
+    category = models.CharField(max_length=3, choices=categories)
+    day = models.CharField(max_length=3, choices=days)
