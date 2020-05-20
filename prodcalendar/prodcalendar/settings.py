b0e2250a44be726d0f70533209bc55c088ab7ff6
piotrfer <piotrek99f@gmail.com>
2020-05-20 15:15:56 +0200

    basic authorization implementation

--- prodcalendar/prodcalendar/settings.py
+++ prodcalendar/prodcalendar/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
     'apps.activity',
+    'apps.authapp',
 ]
 
 MIDDLEWARE = [
@@ -84,6 +87,18 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5432',
     }
+}
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSIONS_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
 
 
