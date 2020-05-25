5cff12dddcc5053be78341fdf58ebb60ef4c48fd
piotrfer <piotrek99f@gmail.com>
2020-05-25 17:12:40 +0200

    put, get and delete activity via api

--- prodcalendar/apps/activity/urls.py
+++ prodcalendar/apps/activity/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', ActivitiesListView.as_view())
+    path('', ActivitiesListView.as_view()),
+    path('<int:id>', ActivitiesDetailView.as_view()),
 ]
--- prodcalendar/apps/activity/views.py
+++ prodcalendar/apps/activity/views.py
@@ -4,6 +4,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 
 from .serializers import ActivitiesSerializer
+from .models import Activity
 
 class ActivitiesListView(APIView):
     permissions_classes = [IsAuthenticated]
@@ -12,3 +13,44 @@ class ActivitiesListView(APIView):
         activities = request.user.activities.all()
         seriliazer = ActivitiesSerializer(activities, many=True)
         return Response(seriliazer.data)
+
+    def post(self, request):
+        request.data['user'] = request.user.pk
+        serializer = ActivitiesSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:  
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ActivitiesDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, id):
+        try:
+            activity = request.user.activities.all().get(id=id)
+        except Activity.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = ActivitiesSerializer(activity)
+        return Response(serializer.data)
+
+
+    def put(self, request, id):
+        try:
+            activity = request.user.activities.all().get(id=id)
+        except Activity.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = ActivitiesSerializer(activity, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:  
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        try:
+            activity = request.user.activities.all().get(id=id)
+        except Activity.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        activity.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
